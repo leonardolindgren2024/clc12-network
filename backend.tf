754e8b30f07d234ee46f198301ce2287e8e4594c
EC2 Default User <ec2-user@ip-172-31-92-45.ec2.internal>
2024-10-15 23:13:03 +0000

    Change bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clc12-network-leonardo-lindgren"
+    bucket = "clc12-network-leonardo-lindgren2"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
   }
